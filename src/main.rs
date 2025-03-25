6348c80410ee7f29b7b76d6aa63c265e43e3ab17
Sebastian Urbina <sebastian@foxglove.dev>
2025-03-25 12:48:27 -0300

    add basic cpu info

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use std::{
-    // ops::Add,
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
+use sysinfo::System;
+
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool, Ordering},
 };
 
 // CPU Channel
 #[derive(Debug, serde::Serialize, schemars::JsonSchema)]
 struct CpuStats {
-    load: f32,
+    usage: f32,
+    physical_cores: u16,
+    core_usage: Vec<f32>,
 }
 foxglove::static_typed_channel!(pub(crate) CPU, "/cpu", CpuStats);
 
-fn log_message() {
+fn log_cpu_info(sys: &mut System) {
+    sys.refresh_cpu_all();
+
     // read the system data
-    CPU.log(&CpuStats { load: 1.0 });
+    CPU.log(&CpuStats {
+        usage: sys.global_cpu_usage(),
+        physical_cores: System::physical_core_count(&sys)
+            .map(|c| c.to_string())
+            .unwrap_or_else(|| "Unknown".to_owned())
+            .trim()
+            .parse()
+            .unwrap_or(0),
+        core_usage: sys.cpus().iter().map(|c| c.cpu_usage()).collect(),
+    });
 }
 
 fn main() {
@@ -35,8 +47,11 @@ fn main() {
         .start_blocking()
         .expect("Server failed to start");
 
+    let mut system = System::new_all();
+    system.refresh_all();
+
     while !done.load(Ordering::Relaxed) {
-        log_message();
+        log_cpu_info(&mut system);
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
